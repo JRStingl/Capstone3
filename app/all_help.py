af629c0665b59372f1323cff0014b71a35fb2cd7
JRStingl <jstingl07@alumni.uwosh.edu>
2021-08-08 13:07:32 -0500

    Updating .py files

--- app/all_help.py
+++ app/all_help.py
@@ -10,14 +10,14 @@ from requests import get
 def get_pickle(imdbid, imdb_movies):
     ind = imdb_movies[imdb_movies.title_id==imdbid].index[0]
     lst = []
-    for i in range(1,56):
+    for i in range(1,100):
         lst.append((i*1433)-1)
         # lst.append((i*1563)-1)
     for i in range(len(lst)):
         if ind < lst[i]:
             files = (lst[i])+1
             break
-    dbfile = open(f'../distance/weights_full_set{int(files)}.pkl', 'rb')
+    dbfile = open(f'../distance/translated_weights_full_set{int(files)}.pkl', 'rb')
     # dbfile = open(f'../distance/full_set{int(files)}.pkl', 'rb')
     # dbfile = open(f'../chunks/imdb_test{int(files)}.pkl', 'rb')
     # source, destination
@@ -33,11 +33,14 @@ def get_netflix_link(title):
     link = "https://www.netflix.com/title/" + lst[1].split('&')[0]
     return link
 
-def get_imdb_link(title):
+def get_imdb_link(title, c_type, c_year):
     title = "+".join(title.split())
-    raw = get(f"https://www.google.com/search?q=netflix+{title}").text
+    title = title +"+"+ c_type +"+"+ c_year
+    print(title)
+    raw = get(f"https://www.google.com/search?q=imdb+{title}").text
     page = fromstring(raw)
     lst = raw.split("https://www.imdb.com/title/")
+    print(len(lst))
     link = "https://www.imdb.com/title/" + lst[1].split('/')[0]
     return link, lst[1].split('/')[0]
 
@@ -53,8 +56,9 @@ def clean_up(movie_min, message, test, year, duration, toy_story, imdb_movies, p
     if genre == '':
         genre = ' '
     looking_for=10
-    print(genre)
-    m_test = imdb_movies[(imdb_movies.original_title==test)&(imdb_movies.year==year)].title_id.item()
+#     print(genre)
+    m_test = imdb_movies[(imdb_movies.original_title==test)&(imdb_movies.year==year)].title_id.values[0]
+    print(m_test)
     matrix, ind = get_pickle(m_test, imdb_movies)
     dex = (ind-np.argsort(matrix[0])[0])
     recommends = np.argsort(matrix[dex])[1:]
@@ -64,13 +68,16 @@ def clean_up(movie_min, message, test, year, duration, toy_story, imdb_movies, p
     posters = []
     ids = imdb_movies.iloc[recommends][(imdb_movies.iloc[recommends].genre.str.contains(genre))].title_id[0:looking_for].values
     new_ids=[]
-    for id, title in zip(ids,top_ten):
-        if id[0]=='s':
+    for m_id, title in zip(ids,top_ten):
+        if m_id[0]=='s':
             # _, imdb_id = get_imdb_link(imdb_movies[imdb_movies.title_id==id].original_title)
-            _, imdb_id = get_imdb_link(title)
+            c_type=imdb_movies[imdb_movies.title_id==m_id].type
+            c_year=imdb_movies[imdb_movies.title_id==m_id].year
+            print(m_id, title, year)
+            _, imdb_id = get_imdb_link(title, c_type, c_year)
             new_ids.append(imdb_id)
         else:
-            new_ids.append(id)
+            new_ids.append(m_id)
     ids = new_ids      
     for vals in ids:
 #     for vals in imdb_movies.iloc[recommends].imdb_title_id[0:10].values:

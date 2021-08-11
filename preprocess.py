51e8d342a09b9c506fb0ff34cd9e965260378424
JRStingl <jstingl07@alumni.uwosh.edu>
2021-08-11 14:53:58 -0500

    Adding data preprocessing python file

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,29 @@
-import pickle
 import pandas as pd
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import pairwise_distances, pairwise_distances_chunked
-from scipy.sparse import vstack
-from nltk.corpus.reader.wordnet import NOUN
-from nltk.corpus import wordnet
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 
 
+
+if __name__ == "__main__":
+    # load data from CSVs into pandas dataframes
+    netflix = pd.read_csv("data/netflix_titles.csv")
+    imdb_movies = pd.read_csv("data/IMDb movies.csv", low_memory=False)
+
+    # rename features for clarity
+    netflix.rename(columns = {'show_id': 'title_id', 'title': 'original_title','release_year': 'year', 'listed_in': 'genre','cast': 'actors' },inplace=True)
+    imdb_movies.rename(columns = {'imdb_title_id': 'title_id'},inplace=True)
+
+    # select features intended for use
+    imdb_cutdown = imdb_movies[['title_id','type','original_title','director','actors','year','duration','genre','description']].copy()
+    netflix_cutdown = netflix[['title_id','type','original_title','director','actors','year','duration','genre','description']].copy()
+
+    # combine the dataframes
+    total_list = imdb_cutdown.copy()
+    for i in range(netflix_cutdown.shape[0]):
+        total_list=total_list.append(netflix_cutdown.iloc[i])
+
+    # reset index and drop old index
+    total_list = total_list.reset_index()
+    total_list.drop('index',inplace=True, axis=1)
+
+    # output combined dateframe as a CSV for future use
+    total_list.to_csv("Combined_Netflix_IMDb_Useful_Columns.csv")
